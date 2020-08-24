45e8b5f2e529df27189e5ecd634d022096611da6
Teodor G <grigor.teodor@gmail.com>
2020-08-24 23:21:04 +0300

    added maven file

--- app/src/main/java/com/zeoflow/app/MainActivity.java
+++ app/src/main/java/com/zeoflow/app/MainActivity.java
@@ -1,12 +1,7 @@
 package com.zeoflow.app;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +9,7 @@ import com.zeoflow.password.strength.PasswordType;
 
 import static com.zeoflow.password.strength.PasswordStrength.initializePassChecker;
 
-public class MainActivity extends AppCompatActivity implements TextWatcher
+public class MainActivity extends AppCompatActivity
 {
 
     @Override
@@ -23,46 +18,10 @@ public class MainActivity extends AppCompatActivity implements TextWatcher
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText password = (EditText) findViewById(R.id.login_password);
-        password.addTextChangedListener(this);
-
         PasswordType res = initializePassChecker()
-                .calculateStrength("(TeEl!Be_Ro@01+25)");
-        Log.d("tag", String.valueOf(res));
-        Log.d("tag", String.valueOf(res));
+                .calculateStrength("3454g36v5n4vt45j6");
+        Toast.makeText(this, String.valueOf(res), Toast.LENGTH_SHORT).show();
 
-    }
-
-    @Override
-    public void afterTextChanged(Editable s)
-    {
-    }
-
-    @Override
-    public void beforeTextChanged(
-            CharSequence s, int start, int count, int after)
-    {
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count)
-    {
-        updatePasswordStrengthView(s.toString());
-    }
-
-    private void updatePasswordStrengthView(String password)
-    {
-
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        TextView strengthView = (TextView) findViewById(R.id.password_strength);
-        if (TextView.VISIBLE != strengthView.getVisibility())
-            return;
-
-        if (password.isEmpty())
-        {
-            strengthView.setText("");
-            progressBar.setProgress(0);
-        }
     }
 }
 
